3d3224bb05550f0bbd579d8a9cdeb9d3d996cb5b
Eliot Delachaux <eliot.delachaux@gmail.com>
2025-12-11 12:05:53 +0100

    figure and convergence

--- TP 1/fluxonium_3_ways.py
+++ TP 1/fluxonium_3_ways.py
@@ -8,7 +8,7 @@ plt.close("all")
 # scqubit
 
 
-def get_eigen(flux, EJ=8.9, EC=2.5, EL=0.5, cutoff=110):
+def get_eigen_sc(flux, EJ=8.9, EC=2.5, EL=0.5, cutoff=110):
     fluxonium = scqubits.Fluxonium(
         EJ=EJ, EC=EC, EL=EL, flux=flux / (2 * np.pi), cutoff=cutoff
     )
@@ -23,7 +23,7 @@ E_2 = []
 E_3 = []
 flux = np.linspace(0, 1, 100)
 for phi_ext in flux:
-    e1, e2, e3 = get_eigen(flux=phi_ext)
+    e1, e2, e3 = get_eigen_sc(flux=phi_ext)
     E_1.append(e1), E_2.append(e2), E_3.append(e3)
 
 plt.figure(1)
@@ -39,7 +39,7 @@ plt.legend()
 # qutip
 
 
-def get_eigen(flux_val, EJ=8.9, EC=2.5, EL=0.5, cutoff=110):
+def get_eigen_qu(flux_val, EJ=8.9, EC=2.5, EL=0.5, cutoff=110):
     phi_ext = flux_val
 
     omega = np.sqrt(8 * EL * EC)
@@ -60,7 +60,7 @@ flux_list = np.linspace(0, 1, 100)
 E_1, E_2, E_3 = [], [], []
 
 for f in flux_list:
-    e1, e2, e3 = get_eigen(f)
+    e1, e2, e3 = get_eigen_qu(f)
     E_1.append(e1)
     E_2.append(e2)
     E_3.append(e3)
@@ -78,7 +78,7 @@ plt.legend()
 # Numpy
 
 
-def get_eigen(flux_val, EJ=8.9, EC=2.5, EL=0.5, cutoff=110):
+def get_eigen_np(flux_val, EJ=8.9, EC=2.5, EL=0.5, cutoff=110):
     phi_ext = flux_val
 
     omega = np.sqrt(8 * EL * EC)
@@ -109,7 +109,7 @@ flux_list = np.linspace(0, 1, 100)
 E_1, E_2, E_3 = [], [], []
 
 for f in flux_list:
-    e1, e2, e3 = get_eigen(f)
+    e1, e2, e3 = get_eigen_np(f)
     E_1.append(e1)
     E_2.append(e2)
     E_3.append(e3)
@@ -123,5 +123,50 @@ plt.ylabel(r"$E^{(n)}-E^{(0)}$ [GHz]")
 plt.title("Résolution avec Numpy")
 plt.legend()
 
+plt.show()
 
+
+# Comparaison de convergence
+
+cutoff_min = 6
+cutoff_max = 110
+CUT = np.arange(cutoff_min,cutoff_max)
+true_value = get_eigen_sc(flux = 0,cutoff = 30)
+Res = np.zeros(
+    (3, 3, cutoff_max-cutoff_min)
+)  # indice i indique le niveau d'énergie, indice j la méthode utilisee et indice k le cutoff
+for cut in CUT:
+    E_sc = get_eigen_sc(flux = 0, cutoff=cut) - true_value
+    E_qu = get_eigen_qu(flux_val=0, cutoff=cut) - true_value
+    E_np = get_eigen_np(flux_val=0, cutoff=cut) - true_value
+    Res[:, :, cut-cutoff_min] = np.array([E_sc, E_qu, E_np]).T
+
+plt.figure(4)
+plt.plot(CUT,Res[0][0][:],label = r'Methode sc')
+plt.plot(CUT,Res[0][1][:],label = r'Methode qutip')
+plt.plot(CUT,Res[0][2][:],label = r'Methode numpy')
+plt.xlabel(r"cutoff")
+plt.ylabel(r"$E^{(1)}-E^{(0)}$ [GHz]")
+plt.title(r'Comparaison de la convergence pour $E^{(1)} - E^{(0)}$')
+plt.legend()
+plt.show()
+
+plt.figure(5)
+plt.plot(CUT,Res[1][0][:],label = r'Methode sc')
+plt.plot(CUT,Res[1][1][:],label = r'Methode qutip')
+plt.plot(CUT,Res[1][2][:],label = r'Methode numpy')
+plt.xlabel(r"cutoff")
+plt.ylabel(r"$E^{(2)}-E^{(0)}$ [GHz]")
+plt.title(r'Comparaison de la convergence pour $E^{(2)} - E^{(0)}$')
+plt.legend()
+plt.show()
+
+plt.figure(6)
+plt.plot(CUT,Res[2][0][:],label = r'Methode sc')
+plt.plot(CUT,Res[2][1][:],label = r'Methode qutip')
+plt.plot(CUT,Res[2][2][:],label = r'Methode numpy')
+plt.xlabel(r"cutoff")
+plt.ylabel(r"$E^{(3)}-E^{(0)}$ [GHz]")
+plt.title(r'Comparaison de la convergence pour $E^{(3)} - E^{(0)}$')
+plt.legend()
 plt.show()
